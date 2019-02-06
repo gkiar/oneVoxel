a94eb420abe250f7a70e1ab8ebfc51e15926ed4b
gkiar <gkiar07@gmail.com>
2019-02-06 11:49:15 -0500

    added json container for noise metadata; supported strictly int16 indices as reqd by json

--- oneVoxel.py
+++ oneVoxel.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python
 
 from argparse import ArgumentParser
-import nibabel as nib
-from nilearn import image as nilimage
-import numpy as np
 from itertools import product
-from scipy import ndimage
 from copy import deepcopy
+import os.path as op
+import operator
+import json
+import uuid
+
+from nilearn import image as nilimage
+from scipy import ndimage
+import nibabel as nib
+import numpy as np
 
 
 def one_voxel_noise(image, mask, scale=True, intensity=0.01, erode=3,
@@ -110,7 +115,7 @@ def one_voxel_noise(image, mask, scale=True, intensity=0.01, erode=3,
             if location:
                 return location
             index = np.random.randint(0, high=len(mask_locs))
-            return mask_locs[index]
+            return tuple(int(ml) for ml in mask_locs[index])
         return location_getter
     location_getter = create_location_getter(location, mask_locs)
 
@@ -131,7 +136,7 @@ def one_voxel_noise(image, mask, scale=True, intensity=0.01, erode=3,
 
     # If single, generate a single index for remaining dimensions
     elif mode == "single":
-        extra_loc = tuple(np.random.randint(0, high=n)
+        extra_loc = tuple(int(np.random.randint(0, high=n))
                           for n in image.shape[len(location_getter()):])
         loc = location_getter() + extra_loc
         image[loc] = noise_injector(image[loc])
@@ -141,7 +146,8 @@ def one_voxel_noise(image, mask, scale=True, intensity=0.01, erode=3,
         # In this case only, the location will be a list of positions
         loc = []
         extra_range = image.shape[len(location_getter()):]
-        extra_range = [list(np.arange(er)) for er in extra_range]
+        extra_range = [list(int(rangeval) for rangeval in np.arange(er))
+                       for er in extra_range]
         extra_locs = product(*extra_range)
         for extra_loc in extra_locs:
             loc += [location_getter() + extra_loc]
@@ -152,10 +158,11 @@ def one_voxel_noise(image, mask, scale=True, intensity=0.01, erode=3,
 
 def make_descriptor(parser, arguments=None):
     import boutiques.creator as bc
-    import os.path as op
-    import json
 
-    desc = bc.CreateDescriptor(parser, execname=op.basename(__file__))
+    desc = bc.CreateDescriptor(parser, execname=op.basename(__file__),
+                               tags={"domain": ["neuroinformatics",
+                                                "image processing",
+                                                "mri", "noise"]})
     basename = op.splitext(__file__)[0]
     desc.save(basename + ".json")
 
@@ -168,7 +175,9 @@ def make_descriptor(parser, arguments=None):
 
 
 def main():
-    parser = ArgumentParser(__file__)
+    parser = ArgumentParser(__file__,
+                            description="Adds noise to a single voxel within an"
+                                        " image, conditioned by an image mask.")
     parser.add_argument("image_file",
                         help="Nifti image to be injected with one-voxel noise. "
                              "Default behaviour is that this will be done at a "
@@ -178,10 +187,9 @@ def main():
                              "input image. The noise location will be selected "
                              "randomly within this mask, unless a location is "
                              "provided.")
-    parser.add_argument("output_file",
-                        help="Nifti image for the perturbed image with one "
-                             "voxel noise. If you want to edit in place, "
-                             "provide the name of the image_file again here.")
+    parser.add_argument("output_directory",
+                        help="Path for where the resulting Nifti image with one"
+                             " voxel noise will be stored.")
     parser.add_argument("--scale", "-s", action="store_true",
                         help="Dictates the way in which noise is aplpied to the"
                              " image. If not set, the value specified with the "
@@ -191,7 +199,7 @@ def main():
     parser.add_argument("--intensity", "-i", action="store", type=float,
                         default=0.01,
                         help="The intensity of the noise to be injected in the "
-                             "image. Default value is 1.01 so specifying the "
+                             "image. Default value is 0.01 so specifying the "
                              "scale flag alone will result in a 1%% intensity "
                              "change at the target location.")
     parser.add_argument("--erode", "-e", action="store", type=int,
@@ -207,11 +215,23 @@ def main():
     parser.add_argument("--force", "-f", action="store_true",
                         help="Disables checks and restrictions on noise that "
                              "may be not recommended for a typical workflow. By"
-                             " default, locations can only be specified in the "
-                             "first 3 dimensions and will be applied uniformly "
-                             "across the rest - this option and a higher "
-                             "dimensional mask or location enables injection at"
-                             "specific points in higher dimensions.")
+                             " default, locations can only be specified within "
+                             "the mask, but this overrides that behaviour.")
+    parser.add_argument("--mode", "-m", action="store",
+                        choices=["single", "uniform", "independent"],
+                        default="single",
+                        help="Determines where noise will be injected in the "
+                             "case of higher-dimensional images than masks. "
+                             "'Single' (default) will choose a single position "
+                             "in all higher dimensions, resulting in 1 point of"
+                             " noise. 'Uniform' will choose a location within "
+                             "the mask and apply it uniformly across all other "
+                             "dimensions. 'Independent' will generate a random "
+                             "location within the mask for each volume in the "
+                             "remaining dimensions, and is mutually exclusive "
+                             "with providing a location.")
+    parser.add_argument("--verbose", "-v", action="store_true",
+                        help="Toggles verbose output printing.")
     parser.add_argument("--boutiques", action="store_true",
                         help="Toggles creation of a Boutiques descriptor and "
                              "invocation from the tool and inputs.")
@@ -226,12 +246,14 @@ def main():
     # Grab arguments from parser
     image = results.image_file
     mask = results.mask_file
-    output = results.output_file
+    output = results.output_directory
     scale = results.scale
     intensity = results.intensity
     erode = results.erode
     location = results.location
     force = results.force
+    mode = results.mode
+    verb = results.verbose
 
     # Load nifti images and extract their data
     image_loaded = nib.load(image)
@@ -243,7 +265,8 @@ def main():
     # Add 1-voxel noise with provided parameters
     output_data, loc = one_voxel_noise(image_data, mask_data, scale=scale,
                                        intensity=intensity, erode=erode,
-                                       location=location, force=force)
+                                       location=location, force=force,
+                                       mode=mode)
 
     # Save noisy image in the same format as the original
     if image_loaded.header_class == nib.Nifti1Header:
@@ -253,20 +276,40 @@ def main():
     else:
         raise TypeError("Unrecognized header - only Nifti is supported.")
 
-    # Create and save the output image
+    # Create and save the output Nifti
     output_loaded = imtype(output_data,
                            header=image_loaded.header,
                            affine=image_loaded.affine)
-    nib.save(output_loaded, output)
 
-    # Add one to make it 1-indexed for the user
-    mm_loc = nilimage.coord_transform(loc[0],
-                                      loc[1],
-                                      loc[2],
-                                      image_loaded.affine)
-    mm_loc += tuple(l+1 for l in loc[3:])
-    print("Noise added in matrix coordinates at: {0}".format(loc))
-    print("Noise added in mm coordinates at: {0}".format(mm_loc))
+    # Convert to mm coordinates for the user
+    if isinstance(loc, tuple):
+        loc = [loc]
+    mm_loc = []
+    for l in loc:
+        tmp_mm = nilimage.coord_transform(l[0], l[1], l[2], image_loaded.affine)
+        mm_loc += [tuple(float(tmm) for tmm in tmp_mm)]
+
+    # Create output filename and save image
+    bname = op.basename(image).split(".")[0]
+    modifier = "_1vox-" + str(uuid.uuid1())[0:8]
+    output_file = op.join(output, bname + modifier)
+
+    # Save image to a Nifti file
+    nib.save(output_loaded, output_file + ".nii.gz")
+
+    # Save noise information to a JSON file
+    with open(output_file + ".json", 'w') as fhandle:
+        noisedict = {"voxel_location": loc,
+                     "mm_location": mm_loc,
+                     "base_image": image,
+                     "scale": scale,
+                     "intensity": intensity}
+        fhandle.write(json.dumps(noisedict, indent=4, sort_keys=True))
+
+    if verb:
+        print("Noise added in matrix coordinates at: {0}".format(loc))
+        print("Noise added in mm coordinates at: {0}".format(mm_loc))
+        print("Image stored in: {0}".format(output_file))
 
 
 if __name__ == "__main__":
